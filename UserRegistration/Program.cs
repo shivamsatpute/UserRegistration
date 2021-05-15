b754a5d4747b0ab0a5f5d0875c23ee83610cc963
shivam satpute <shivam897satpute@gmail.com>
2021-05-15 22:27:11 +0530

    [shivam]add: First Name Valid

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        public static bool NameValidation(String firstName)
+        {
+            String pattern = "^[A-Z]{1,}[a-z]{2,}$";  
+
+            Regex regex = new Regex(pattern);      
+            return regex.IsMatch(firstName);    
+        }
+        static void Main(string[] args)  
+        {
+            Console.WriteLine("Welcome to User Registration");       
+
+            Console.WriteLine("Enter the First Name");  
+            String firstName = Console.ReadLine();     
+
+            if (NameValidation(firstName))  
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+
+        }
+    }
+}
